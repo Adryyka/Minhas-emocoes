1972241e8cd23f653693197142cf714f10d7d1af
Adriana Rocha <adrirocha.cg@gmail.com>
2024-09-11 20:18:53 +0100

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,58 @@
+let dados = [
+    {
+        titulo: "Alegria",
+        descricao: "A alegria é uma emoção básica e universal que se manifesta como uma sensação de prazer, felicidade e bem-estar. É uma resposta emocional positiva a eventos ou situações que consideramos agradáveis, satisfatórias ou significativas.",
+        link: "https://youtu.be/LSpeM7G4zfY?si=w1unyBWkwmcGwNAs",
+        tags: "alegre feliz contente motivado"
+    },
+    {
+        titulo: "Tristeza",
+        descricao: "A tristeza é uma emoção básica e universal que todos nós experimentamos em algum momento da vida. É uma resposta emocional natural a eventos que percebemos como perdas, decepções ou frustrações.",
+        link: "https://youtu.be/bZrgANDg4hA?si=RZ2r1v4ntRaKA1S_",
+        tags: "triste infeliz solitario dor desanimado"
+    },
+     
+         {
+         titulo: "Nojinho",
+        descricao: "O nojo é uma emoção básica e universal que experimentamos quando algo nos causa repulsa ou aversão. É uma reação natural a estímulos que percebemos como sujos, impuros, tóxicos ou moralmente reprováveis.",
+        link: "https://youtu.be/9qIYJfCOHec?si=svm0AbVnYzjcN-vR",
+        tags: "Nojo enjoado enojado"
+     },
+     {
+        titulo: "Medo",
+        descricao: "O medo é uma emoção fundamental que todos experimentamos em algum momento da vida. É uma resposta natural a situações que percebemos como perigosas ou ameaçadoras, seja para nossa integridade física ou emocional.",
+        link: "https://youtu.be/uboHnAhb63E?si=hCzHOvrdMTEv86Pk",
+        tags: "medroso apavorado "
+    },
+     {
+        titulo: "Raiva",
+        descricao: "A raiva é uma emoção básica e universal que experimentamos todos nós em algum momento da vida. Ela se manifesta como uma resposta a situações que percebemos como injustas, ameaçadoras ou frustrantes.",
+        link: "https://youtu.be/XzUWi7F-kG8?si=7HP98UL_7cUS0jji",
+        tags: "ira odio rancoroso irritado "
+    },
+     {
+         titulo: "Ansiedade",
+         descricao: "A ansiedade é uma emoção natural e comum que todos nós experimentamos em algum momento da vida. Ela pode ser entendida como um estado de alerta, uma preparação para enfrentar desafios ou situações que percebemos como ameaçadoras.",
+         link: "https://youtu.be/MUd5bWyDyYI?si=VPawgAhyfZru8w6N",
+         tags: "ansioso nervoso "
+    },
+     {
+        titulo: "Inveja",
+         descricao: "A inveja é um sentimento que surge quando desejamos algo que outra pessoa possui, muitas vezes acompanhado de um sentimento de inferioridade. É como se olhássemos para o outro e pensássemos: Eu queria ter isso também, e por que eu não tenho?",
+         link: "https://youtu.be/8w4z6PILNKI?si=VJF3lEOU7WTQ5s9g",
+         tags: "invejoso "
+     },
+     {
+         titulo: "Tedio",
+         descricao: "O tédio é uma emoção complexa que se manifesta como um sentimento de desinteresse, aborrecimento e vazio. É uma experiência subjetiva que surge quando não encontramos estímulos suficientes para engajar nossa mente e nos manter motivados.",
+         link: "https://youtu.be/LfhyVL1Af-g?si=TmCLixJRIuumoFX4",
+         tags: "cansado preguiçoso preguiça "
+     },
+     {
+         titulo: "Vergonha",
+         descricao: "A vergonha é uma emoção complexa que envolve um sentimento de inadequação, autoconsciência e vulnerabilidade. É uma experiência subjetiva que surge quando percebemos que não correspondemos aos nossos próprios padrões ou aos padrões sociais, ou quando tememos ser julgados negativamente por outros.",
+         link: "https://youtu.be/gsmGm7phzgI?si=MD1kuoQQy79IEOjE",
+         tags: "tímido envergonhado "
+     },
+];
+  
